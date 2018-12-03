7f7cb4bd5120cf073802d577588317c373e5cfb2
Sunny Ahmed <sunny.ahmed7575@gmail.com>
2018-12-03 20:32:33 +0500

    Initial Commit

---
+++ InventoryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InventoryApp
+//
+//  Created by sunny on 03/12/2018.
+//  Copyright © 2018 sunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
